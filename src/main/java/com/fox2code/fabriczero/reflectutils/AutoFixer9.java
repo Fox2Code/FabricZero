6d4542ab8a91e273a08eb47f339aee58052ae971
Fox2Code <fox2code@gmail.com>
2020-11-27 17:45:36 +0100

    Fix Java8 Compile + Add mods rules

--- src/main/java/com/fox2code/fabriczero/reflectutils/AutoFixer9.java
+++ src/main/java/com/fox2code/fabriczero/reflectutils/AutoFixer9.java
@@ -1,10 +1,16 @@
 package com.fox2code.fabriczero.reflectutils;
 
 import java.util.Objects;
+import java.util.Optional;
 
 public class AutoFixer9 {
-    public static String getCommandLine() {
-        return Objects.requireNonNull(ProcessHandle.current())
-                .info().commandLine().orElseThrow(NullPointerException::new);
+    @SuppressWarnings("unchecked")
+    public static String getCommandLine() throws ReflectiveOperationException {
+        final Class<?> ProcessHandle = Class.forName("java.lang.ProcessHandle");
+        final Class<?> ProcessHandle$Info = Class.forName("java.lang.ProcessHandle$Info");
+        return ((Optional<String>)ProcessHandle$Info.getDeclaredMethod("commandLine").invoke(
+                ProcessHandle.getDeclaredMethod("info").invoke(
+                Objects.requireNonNull(ProcessHandle.getDeclaredMethod("current").invoke(null)))
+                )).orElseThrow(NullPointerException::new);
     }
 }
